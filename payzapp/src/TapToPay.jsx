db91ec766b60736b8d9ab3599e0b5ece0e5e4688
srikar.h <avinash.b0802@gmail.com>
2023-03-03 14:49:39 +0530

    textinputs

--- payzapp/src/TapToPay.jsx
+++ payzapp/src/TapToPay.jsx
@@ -1,10 +1,32 @@
+import { Grid, Paper } from "@mui/material";
+import CurrencyPoundIcon from '@mui/icons-material/CurrencyPound';
 import React from "react";
 import AppContext from "./AppContext";
+import Euro from "@mui/icons-material/Euro";
+import { styled } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
 
 function TapToPay() {
-    const value = React.useContext(AppContext);
-    console.log(value)
-    return(<p>tap to pay</p>)
+    const { amount } = React.useContext(AppContext);
+    return (
+        <>
+            <Grid container>
+                <Grid item xs={6}>
+                    <Item>
+                        <b>Transaction Amount</b>
+                        <CurrencyPoundIcon size="small"/>{amount}
+                    </Item>
+                </Grid>
+            </Grid>
+        </>
+    );
 }
 
 export default TapToPay;
