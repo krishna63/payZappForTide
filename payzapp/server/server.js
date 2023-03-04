7742c99fe3eea211210f6456178bb3c67d187616
srikar.h <avinash.b0802@gmail.com>
2023-03-04 10:44:02 +0530

    data communication

--- payzapp/server/server.js
+++ payzapp/server/server.js
@@ -1,23 +1,38 @@
 const http = require('http');
 const url = require("url");
+const { v4: uuidv4 } = require("uuid")
+let data = require("./data");
 const hostname = '127.0.0.1';
 const port = 8080;
 
 const server = http.createServer((req, res) => {
 
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Access-Control-Allow-Origin','*')
-  res.setHeader('Access-Control-Allow-Headers','*')
-  res.setHeader('Access-Control-Request-Headers','*');
+    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Headers', '*')
+    res.setHeader('Access-Control-Request-Headers', '*');
 
-  res.statusCode = 200;
-    const reqUrl = url.parse(req.url).pathname
-    if(reqUrl == "/lessThanTen") {
-        res.write(JSON.stringify({a:'a'}))
-        res.end()
+    res.statusCode = 200;
+    const reqUrl = url.parse(req.url).pathname;
+    let amount = 0;
+    if (reqUrl == "/lessThanTen") {
+        req.on('data', (data) => {
+            amount = JSON.parse(data.toString()).amount;
+        })
+        req.on('end', () => {
+            const trx_id = uuidv4();
+            const new_trx = {
+                trx_id: trx_id,
+                amount: amount,
+                date: new Date()
+            }
+            data = { ...data, new_trx };
+            res.write(JSON.stringify(new_trx));
+            res.end()
+        })
     }
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
